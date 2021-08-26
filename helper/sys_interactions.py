79c6eabb02bdb973fd355686644f6e0e0ab1a434
Saba <narmiabas@gmail.com>
2021-08-25 18:16:09 -0700

    Updated logs on 2021-08-25T18:16:09.789263

--- helper/sys_interactions.py
+++ helper/sys_interactions.py
@@ -3,6 +3,7 @@ import socket
 
 ### Determine whether the user is connected to the internet.
 def is_online():
+    return True
     try:
         conn = socket.create_connection(("www.google.com", 80), timeout=1)
         if conn == None:
